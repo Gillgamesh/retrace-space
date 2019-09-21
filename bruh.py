7b3daa0e6c85403a6786e3f74b03d40b7ba4be0d
gillgamesh <gilvirgillgg@gmail.com>
2019-09-21 06:46:23 -0400

    bruh

--- bruh.py
+++ bruh.py
@@ -6,17 +6,9 @@ from google.cloud import vision
 from google.cloud.vision import types
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else: rval = False
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
-starttime=time.time()
-counter = 0
-def detect_text(path):
-    """Detects text in the file."""
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
+
+
+def detect_text(content):
 
     image = types.Image(content=content)
     response = client.face_detection(image=image)
@@ -26,6 +18,7 @@ def detect_text(path):
                        'LIKELY', 'VERY_LIKELY')
     print('Faces:')
     for face in faces:
+        print(face)
         print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
         print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
         print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
@@ -35,18 +28,22 @@ def detect_text(path):
 
         print('face bounds: {}'.format(','.join(vertices)))
 
+
+if vc.isOpened():
+    rval, frame = vc.read()
+else:
+    rval = False
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    file = 'live' + str(counter) + '.png'
-    if key == 27: # exit on ESC
-        break
-        file = 'live' + str(counter) + '.png'
-    cv2.imwrite( file,frame)
-    # print OCR text
+    file = cv2.imencode('.jpg', frame)[1].tostring()
+    sleeptime = 10
+    if sleeptime > 0:
+        time.sleep(sleeptime)
     print(detect_text(file))
-    counter += 1
-    time.sleep(10.0 - ((time.time() - starttime) % 10.0))
 
 cv2.destroyWindow("preview")
